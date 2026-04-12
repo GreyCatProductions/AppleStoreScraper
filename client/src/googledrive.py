83dc849c524cc83c9cdef0e7e5a67e50e69ec3ea
greycatproduction <greycatproductionsoriginal@gmail.com>
2026-04-12 16:49:14 +0200

    bugfix client

--- client/src/googledrive.py
+++ client/src/googledrive.py
@@ -1,6 +1,5 @@
 import os
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
 from io import BytesIO
@@ -18,24 +17,18 @@ class GoogleDriveClient:
         self.service = build("drive", "v3", credentials=creds)
         self.htmlFolderID = htmlFolderID
 
-    def upload_with_conversion(self, name: str, content: str) -> int | None:
+    def upload_with_conversion(self, name: str, content: str) -> str:
         name = re.sub(r'[^\w\-.]', '_', name)[:200]
-        try:
-            file_metadata = {
-                "name": name,
-                "parents": [self.htmlFolderID]
-            }
-            media = MediaIoBaseUpload(BytesIO(content.encode("utf-8")), mimetype="text/html", resumable=True)
-
-            file = (
-                self.service.files()
-                .create(body=file_metadata, media_body=media, fields="id", supportsAllDrives=True)
-                .execute()
-            )
-            logger.info(f'File with ID: "{file.get("id")}" and Name: "{name}" has been uploaded.')
-
-        except HttpError as error:
-            logger.error(f"An error occurred: {error}")
-            file = None
-
-        return file.get("id") if file else None
+        file_metadata = {
+            "name": name,
+            "parents": [self.htmlFolderID]
+        }
+        media = MediaIoBaseUpload(BytesIO(content.encode("utf-8")), mimetype="text/html", resumable=True)
+        file = (
+            self.service.files()
+            .create(body=file_metadata, media_body=media, fields="id", supportsAllDrives=True)
+            .execute()
+        )
+        file_id = file.get("id")
+        logger.info(f'File with ID: "{file_id}" and Name: "{name}" has been uploaded.')
+        return file_id
